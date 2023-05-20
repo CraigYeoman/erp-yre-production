83f7a7bafa6b0e73ceb59189ee48ba547275ed2e
CraigYeoman <yeomancraig@gmail.com>
2023-05-20 07:09:25 -0500

    Moving directories to work with Render.com

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== "production") {
   app.use(logger("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
 app.use(helmet());
